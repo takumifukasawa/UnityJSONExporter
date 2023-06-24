59b22e4a55fcd35f90d247e35f63c96e99d1dd43
takumifukasawa <takumifukasawa@gmail.com>
2023-06-24 21:36:30 +0900

    format

--- Assets/Scripts/WebSocketConnector.cs
+++ Assets/Scripts/WebSocketConnector.cs
@@ -8,28 +8,19 @@ public class WebSocketConnector : MonoBehaviour
 {
     [SerializeField]
     private int _port;
-    
+
     private WebSocket _webSocket;
-    
+
     private async void Start()
     {
         _webSocket = new WebSocket($"ws://localhost:{_port}");
-        
-        _webSocket.OnOpen += () =>
-        {
-            Debug.Log("Connection open!");
-        };
-        
-        _webSocket.OnError += (e) =>
-        {
-            Debug.Log("Error! " + e);
-        };
-        
-        _webSocket.OnClose += (e) =>
-        {
-            Debug.Log("Connection closed!");
-        };
-        
+
+        _webSocket.OnOpen += () => { Debug.Log("Connection open!"); };
+
+        _webSocket.OnError += (e) => { Debug.Log("Error! " + e); };
+
+        _webSocket.OnClose += (e) => { Debug.Log("Connection closed!"); };
+
         _webSocket.OnMessage += (bytes) =>
         {
             Debug.Log("OnMessage!");
@@ -37,27 +28,32 @@ public class WebSocketConnector : MonoBehaviour
             var message = System.Text.Encoding.UTF8.GetString(bytes);
             Debug.Log("OnMessage! " + message);
         };
-        
+
         InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
-        
+
         await _webSocket.Connect();
     }
 
     private void Update()
     {
+#if !UNITY_WEBGL || UNITY_EDITOR
         _webSocket.DispatchMessageQueue();
+#endif
     }
 
     private async void OnApplicationQuit()
     {
         await _webSocket.Close();
     }
-    
-    async void SendWebSocketMessage() {
+
+    async void SendWebSocketMessage()
+    {
         if (_webSocket.State == WebSocketState.Open)
         {
-            await _webSocket.Send(new byte[] { 10, 20, 30 });
-            await _webSocket.SendText("plain text message");
+            float t = 1.2f;
+            byte[] floats = BitConverter.GetBytes(t);
+            await _webSocket.Send(floats);
+            await _webSocket.SendText($"plain text message - t: {t.ToString()}");
         }
     }
 }
