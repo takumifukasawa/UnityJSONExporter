f370abff6bb761b0104548b72a48d124f36a2956
takumifukasawa <takumifukasawa.com>
2023-06-26 20:07:27 +0900

    関数まとめる

--- Assets/UnityJSONExporter/ComponentInfo/PlayableDirectorComponentInfo.cs
+++ Assets/UnityJSONExporter/ComponentInfo/PlayableDirectorComponentInfo.cs
@@ -86,11 +86,6 @@ namespace UnityJSONExporter
             var spf = 1f / fps;
             var frameCount = playableDirector.duration / spf;
 
-
-            // for (int i = 0; i < frameCount; i++)
-            // {
-            //     var currentTime = spf * i;
-
             foreach (var track in timelineAsset.GetOutputTracks())
             {
                 // for debug
@@ -120,78 +115,9 @@ namespace UnityJSONExporter
                     // Debug.Log($"[TestMain] animation track");
                     var animationTrack = track as AnimationTrack;
                     var timelineClips = animationTrack.GetClips();
-                    foreach (var timelineClip in timelineClips)
-                    {
-                        // Debug.Log($"[TestMain] each timeline clip");
-                        var animationClip = timelineClip.animationClip;
-                        if (animationClip == null)
-                        {
-                            continue;
-                        }
 
-                        var animationClipInfo = new AnimationClipInfo();
-                        trackInfo.AnimationClips.Add(animationClipInfo);
-
-                        var bindings = AnimationUtility.GetCurveBindings(animationClip);
-
-                        foreach (var binding in bindings)
-                        {
-                            var animationClipBinding = new AnimationClipBinding();
-                            animationClipInfo.Bindings.Add(animationClipBinding);
-                            animationClipBinding.PropertyName = binding.propertyName;
-
-                            if (binding.type.FullName != typeof(Transform).FullName)
-                            {
-                                continue;
-                            }
-
-                            var curve = AnimationUtility.GetEditorCurve(animationClip, binding);
-
-                            foreach (var key in curve.keys)
-                            {
-                                float keyValue = key.value;
-                                switch (binding.propertyName)
-                                {
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_POSITION_X:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Position, TransformConverter.AxisDirection.X, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_POSITION_Y:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Position, TransformConverter.AxisDirection.Y, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_POSITION_Z:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Position, TransformConverter.AxisDirection.Z, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_EULER_ANGLES_RAW_X:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Rotation, TransformConverter.AxisDirection.X, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_EULER_ANGLES_RAW_Y:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Rotation, TransformConverter.AxisDirection.Y, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_EULER_ANGLES_RAW_Z:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Rotation, TransformConverter.AxisDirection.Z, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_SCALE_X:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Scale, TransformConverter.AxisDirection.X, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_SCALE_Y:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Scale, TransformConverter.AxisDirection.Y, keyValue);
-                                        break;
-                                    case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_SCALE_Z:
-                                        keyValue = TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Scale, TransformConverter.AxisDirection.Z, keyValue);
-                                        break;
-                                    default:
-                                        throw new Exception("invalid params");
-                                }
-
-                                var animationClipKeyframe = new AnimationClipKeyframe();
-                                animationClipKeyframe.Time = key.time;
-                                animationClipKeyframe.Value = keyValue;
-                                animationClipKeyframe.InTangent = key.inTangent;
-                                animationClipKeyframe.OutTangent = key.outTangent;
-                                animationClipBinding.Keyframes.Add(animationClipKeyframe);
-                            }
-                        }
-                    }
+                    var animationClipInfoList = GenerateAnimationClipInfoList(timelineClips, convertAxis, typeof(Transform), true);
+                    trackInfo.AnimationClips = animationClipInfoList;
 
                     continue;
                 }
@@ -202,38 +128,8 @@ namespace UnityJSONExporter
                     // Debug.Log($"[TestMain] light control track");
                     var lightControlTrack = track as LightControlTrack;
                     var timelineClips = lightControlTrack.GetClips();
-                    foreach (var timelineClip in timelineClips)
-                    {
-                        var animationClip = timelineClip.curves;
-                        if (animationClip == null)
-                        {
-                            continue;
-                        }
-
-                        var animationClipInfo = new AnimationClipInfo();
-                        trackInfo.AnimationClips.Add(animationClipInfo);
-
-                        var bindings = AnimationUtility.GetCurveBindings(animationClip);
-
-                        foreach (var binding in bindings)
-                        {
-                            var animationClipBinding = new AnimationClipBinding();
-                            animationClipInfo.Bindings.Add(animationClipBinding);
-                            animationClipBinding.PropertyName = binding.propertyName;
-
-                            var curve = AnimationUtility.GetEditorCurve(animationClip, binding);
-
-                            foreach (var key in curve.keys)
-                            {
-                                var animationClipKeyframe = new AnimationClipKeyframe();
-                                animationClipKeyframe.Time = key.time;
-                                animationClipKeyframe.Value = key.value;
-                                animationClipKeyframe.InTangent = key.inTangent;
-                                animationClipKeyframe.OutTangent = key.outTangent;
-                                animationClipBinding.Keyframes.Add(animationClipKeyframe);
-                            }
-                        }
-                    }
+                    var animationClipInfoList = GenerateAnimationClipInfoList(timelineClips, convertAxis);
+                    trackInfo.AnimationClips = animationClipInfoList;
 
                     continue;
                 }
@@ -242,45 +138,105 @@ namespace UnityJSONExporter
                 if (track.GetType() == typeof(ControlTrack))
                 {
                 }
+            }
+        }
 
-                // tmp
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="timelineClips"></param>
+        /// <param name="convertAxis"></param>
+        /// <param name="checkType"></param>
+        /// <param name="convertTransformValue"></param>
+        /// <returns></returns>
+        List<AnimationClipInfo> GenerateAnimationClipInfoList(
+            IEnumerable<TimelineClip> timelineClips,
+            ConvertAxis convertAxis,
+            Type checkType = null,
+            bool convertTransformValue = false
+        )
+        {
+            var animationClipInfoList = new List<AnimationClipInfo>();
+            foreach (var timelineClip in timelineClips)
+            {
+                // Debug.Log($"[TestMain] each timeline clip");
+                var animationClip = timelineClip.animationClip;
+                if (animationClip == null)
+                {
+                    continue;
+                }
 
-                // // animation track
-                // if (track.GetType() == typeof(AnimationTrack))
-                // {
-                //     Debug.Log($"[TestMain] animation track");
-                //     var animationTrack = track as AnimationTrack;
-                //     var timelineClips = animationTrack.GetClips();
-                //     foreach (var timelineClip in timelineClips)
-                //     {
-                //         // Debug.Log($"[TestMain] each timeline clip");
-                //         var animationClip = timelineClip.animationClip;
-                //         if (animationClip != null)
-                //         {
-                //             var bindings = AnimationUtility.GetCurveBindings(animationClip);
-                //             var animationTrackBinder = new AnimationTrackBinder(animationClip, bindings, currentTime);
-                //         }
-                //     }
+                var animationClipInfo = new AnimationClipInfo();
+                animationClipInfoList.Add(animationClipInfo);
+                // trackInfo.AnimationClips.Add(animationClipInfo);
 
-                //     continue;
-                // }
+                var bindings = AnimationUtility.GetCurveBindings(animationClip);
 
-                // // light control track
-                // if (track.GetType() == typeof(LightControlTrack))
-                // {
-                //     Debug.Log($"[TestMain] light control track");
-                //     var lightControlTrack = track as LightControlTrack;
-                //     var timelineClips = lightControlTrack.GetClips();
-                //     foreach (var timelineClip in timelineClips)
-                //     {
-                //         var animationClip = timelineClip.curves;
-                //         if (animationClip != null)
-                //         {
-                //             var bindings = AnimationUtility.GetCurveBindings(animationClip);
-                //             var lightControlTrackPropertyBinder = new LightControlTrackPropertyBinder(animationClip, bindings, currentTime);
-                //         }
-                //     }
-                // }
+                foreach (var binding in bindings)
+                {
+                    var animationClipBinding = new AnimationClipBinding();
+                    animationClipInfo.Bindings.Add(animationClipBinding);
+                    animationClipBinding.PropertyName = binding.propertyName;
+
+                    // if (binding.type.FullName != typeof(Transform).FullName)
+                    if (checkType != null && binding.type.FullName != checkType.FullName)
+                    {
+                        continue;
+                    }
+
+                    var curve = AnimationUtility.GetEditorCurve(animationClip, binding);
+
+                    foreach (var key in curve.keys)
+                    {
+                        float keyValue = convertTransformValue
+                                             ? ConvertTransformCurveValue(binding, key.value, convertAxis)
+                                             : key.value;
+
+                        var animationClipKeyframe = new AnimationClipKeyframe();
+                        animationClipKeyframe.Time = key.time;
+                        animationClipKeyframe.Value = keyValue;
+                        animationClipKeyframe.InTangent = key.inTangent;
+                        animationClipKeyframe.OutTangent = key.outTangent;
+                        animationClipBinding.Keyframes.Add(animationClipKeyframe);
+                    }
+                }
+            }
+
+            return animationClipInfoList;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="binding"></param>
+        /// <param name="keyValue"></param>
+        /// <param name="convertAxis"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        float ConvertTransformCurveValue(EditorCurveBinding binding, float keyValue, ConvertAxis convertAxis)
+        {
+            switch (binding.propertyName)
+            {
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_POSITION_X:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Position, TransformConverter.AxisDirection.X, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_POSITION_Y:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Position, TransformConverter.AxisDirection.Y, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_POSITION_Z:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Position, TransformConverter.AxisDirection.Z, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_EULER_ANGLES_RAW_X:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Rotation, TransformConverter.AxisDirection.X, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_EULER_ANGLES_RAW_Y:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Rotation, TransformConverter.AxisDirection.Y, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_EULER_ANGLES_RAW_Z:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Rotation, TransformConverter.AxisDirection.Z, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_SCALE_X:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Scale, TransformConverter.AxisDirection.X, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_SCALE_Y:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Scale, TransformConverter.AxisDirection.Y, keyValue);
+                case Constants.ANIMATION_CLIP_PROPERTY_LOCAL_SCALE_Z:
+                    return TransformConverter.ConvertValue(convertAxis, TransformConverter.TransformType.Scale, TransformConverter.AxisDirection.Z, keyValue);
+                default:
+                    throw new Exception("invalid params");
             }
         }
     }
