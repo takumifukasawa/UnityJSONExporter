b57866989148fc644a90b1559d3d4c3988751cb1
takumifukasawa <takumifukasawa@gmail.com>
2024-10-10 22:41:50 +0900

    object lookat track

---
+++ Assets/Scripts/ObjectLookAtTrack/ObjectLookAtAsset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class ObjectLookAtAsset : PlayableAsset
+{
+    // public ExposedReference<Transform> Target;  // Timeline上でExposeできるように設定
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<ObjectLookAtBehaviour>.Create(graph);
+
+        // ObjectLookAtBehaviour behaviour = playable.GetBehaviour();
+        // behaviour.Target = Target.Resolve(graph.GetResolver());
+
+        return playable;
+    }
+}
